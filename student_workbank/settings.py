ac01336e390a046267f17002cc45f7b7b1005ff3
Akanksha <akankshaingale29@gmail.com>
2026-03-01 21:45:53 +0530

    Production settings update

--- student_workbank/settings.py
+++ student_workbank/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-=ghyu5kdh=vq-5fcc^lx_$=xs((x+2q%v##_h$nz&w&pk%vo'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
