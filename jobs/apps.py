519a371ddeaab5e38bc21a5eec620723597a50ac
Akanksha <akankshaingale29@gmail.com>
2026-03-02 10:05:40 +0530

    Remove ready method completely

--- jobs/apps.py
+++ jobs/apps.py
@@ -4,36 +4,8 @@ class JobsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jobs'
 
-    def ready(self):
-        from jobs.models import Job
+   from django.apps import AppConfig
 
-        if Job.objects.count() == 0:
-            Job.objects.create(
-                title="Frontend Developer Intern",
-                company="TechNova Solutions",
-                location="Pune, India",
-                description="Work on responsive UI using React and Tailwind CSS."
-            )
-
-            Job.objects.create(
-                title="Backend Developer Intern",
-                company="CodeSphere Labs",
-                location="Remote",
-                description="Build REST APIs using Django and PostgreSQL."
-            )
-
-            Job.objects.create(
-                title="AI/ML Intern",
-                company="DataMind Analytics",
-                location="Bangalore, India",
-                description="Work on ML models and NLP projects."
-            )
-
-            Job.objects.create(
-                title="Social Media Manager Intern",
-                company="BrandBoost Media",
-                location="Mumbai, India",
-                description="Handle reels strategy and brand campaigns."
-            )
-
-            print("Sample jobs inserted.")
+class JobsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jobs'
